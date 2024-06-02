e77dd1481f66e7434b39f3931271d8966238f4fa
JustBrian <61902485+lbryant-sss@users.noreply.github.com>
2024-06-02 22:59:38 +0300

    Update forms.py

--- backend/api/forms.py
+++ backend/api/forms.py
@@ -5,10 +5,3 @@ class UploadForm(forms.ModelForm):
     class Meta:
         model = UploadModel
         fields = ['data_file_upload', 'table_name']
-
-    def clean_data_file_upload(self):  # Renamed to match the field name
-        file = self.cleaned_data.get('data_file_upload')
-        if file:
-            if not file.name.endswith('.csv'):
-                raise forms.ValidationError("Only .csv files are allowed.")
-        return file
